884f65470b513258346806109973a90254d1c1b7
charlesagyemang <micnkru@gmail.com>
2016-11-23 14:37:53 +0000

    setted up api and passed first test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+
+	#Api definition
+	namespace :api, defaults: { format: :json },
+								constraints: { subdomain: 'api'}, path: '/'	do 
+		
+		scope module: :v1,
+						constraints: ApiConstraints.new(version: 1, default: true)  do
+			#name of resources here
+		
+				
+		end	
+	end	
 
 end
